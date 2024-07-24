e0dbd69468dc7e8351064ed3b633e7e986f13f90
Fitrianto Dwi Cahyo <riandwicahyo28@gmail.com>
2024-07-24 12:04:32 +0700

    feat(ui): add register screen

--- lib/views/screens/auth/register_screen.dart
+++ lib/views/screens/auth/register_screen.dart
@@ -1,17 +1,39 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:shop_test/const/widgets/custom_textfield.dart';
 import 'package:shop_test/controllers/auth_controller.dart';
+import 'package:shop_test/views/screens/auth/login_screen.dart';
 
 class RegisterScreen extends StatelessWidget {
-  RegisterScreen({super.key});
-
-  final AuthController authController = AuthController();
+  final AuthController _authController = Get.put(AuthController());
 
   final GlobalKey<FormState> _formState = GlobalKey<FormState>();
 
-  TextEditingController email = TextEditingController();
-  TextEditingController pass = TextEditingController();
-  TextEditingController fullName = TextEditingController();
+  registerUser() async {
+    if (_authController.image != null) {
+      if (_formState.currentState!.validate()) {
+        String res = await _authController.createNewUser(
+            _authController.email.text,
+            _authController.fullName.text,
+            _authController.pass.text,
+            _authController.image);
+
+        print(res);
+        if (res == 'success') {
+          Get.to(LoginScreen());
+          Get.snackbar('Success', 'Selamat Bergabung');
+        } else {
+          Get.snackbar('Error', res.toString());
+        }
+      } else {
+        print('Not Validate');
+      }
+    } else {
+      Get.snackbar('Error', 'Add Picture');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,33 +45,102 @@ class RegisterScreen extends StatelessWidget {
         padding: const EdgeInsets.all(24.0),
         child: Form(
           key: _formState,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: ListView(
             children: [
-              CustomTextfield(title: 'Email address', isPassword: false,error: 'Email tidak boleh kosong',controller: email,),
+              GetBuilder<AuthController>(
+                init: AuthController(),
+                initState: (_) {},
+                builder: (_) {
+                  return Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      _authController.image == null
+                          ? const CircleAvatar(
+                              radius: 65,
+                              child: Icon(
+                                Icons.person,
+                                size: 56,
+                              ),
+                            )
+                          : CircleAvatar(
+                              radius: 65,
+                              backgroundImage:
+                                  MemoryImage(_authController.image!),
+                            ),
+                      Positioned(
+                        right: 100,
+                        top: 10,
+                        child: CircleAvatar(
+                          backgroundColor: Color.fromARGB(255, 31, 5, 126),
+                          child: IconButton(
+                            onPressed: () {
+                              _authController.selecetedGalleryImage();
+                            },
+                            icon: const Icon(
+                              Icons.add,
+                              size: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
               const SizedBox(
                 height: 24,
               ),
-              CustomTextfield(title: 'Full Name', isPassword: false,error: 'Full Name tidak boleh kosong',controller: fullName,),
+              CustomTextfield(
+                title: 'Email address',
+                isPassword: false,
+                error: 'Email tidak boleh kosong',
+                controller: _authController.email,
+              ),
               const SizedBox(
                 height: 24,
               ),
-              CustomTextfield(title: 'Password', isPassword: true,error: 'Password tidak boleh kosong',controller: pass,),
+              CustomTextfield(
+                title: 'Full Name',
+                isPassword: false,
+                error: 'Full Name tidak boleh kosong',
+                controller: _authController.fullName,
+              ),
               const SizedBox(
                 height: 24,
               ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formState.currentState!.validate()) {
-                      authController.createNewUser(email.text, fullName.text, pass.text);
-                      print('Success');
-                    } else {
-                      print('Not Validate');
-                    }
-                  },
-                  child: const Text('Sign Up'),
+              Obx(
+                () => TextFormField(
+                  controller: _authController.pass,
+                  decoration: InputDecoration(
+                    border: const OutlineInputBorder(),
+                    label: const Text('Password'),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        _authController.isHidden.toggle();
+                      }, 
+                      icon: _authController.isHidden.value
+                          ? const Icon(Icons.remove_red_eye_outlined)
+                          : const Icon(Icons.remove_red_eye),
+                    ),
+                  ),
+                  obscureText: _authController.isHidden.value ? true : false,
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Obx(
+                () => SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _authController.isLoading.value ? null : registerUser();
+                    },
+                    child: _authController.isLoading.value
+                        ? const CircularProgressIndicator()
+                        : const Text('Sign Up'),
+                  ),
                 ),
               ),
             ],
