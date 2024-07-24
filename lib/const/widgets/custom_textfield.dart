8ed0c58ddb170eab9cbbf61aeab236e4e9ae9560
Fitrianto Dwi Cahyo <riandwicahyo28@gmail.com>
2024-07-24 11:28:31 +0700

    feat(ui) : add new custom textfield

--- lib/const/widgets/custom_textfield.dart
+++ lib/const/widgets/custom_textfield.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_rx/get_rx.dart';
+import 'package:shop_test/controllers/other_controller/hide_controller.dart';
 
 class CustomTextfield extends StatelessWidget {
+  final hideController = Get.put(PasswordController());
+
   final String title;
   final String error;
-  
+
   bool isPassword;
-  bool isHidden = false;
+  RxBool isHidden = false.obs;
 
   TextEditingController controller = TextEditingController();
 
@@ -14,31 +19,37 @@ class CustomTextfield extends StatelessWidget {
       required this.isPassword,
       required this.error,
       required this.controller,
-      isHidden,
       super.key});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      controller: controller,
-      validator: (value) {
-        if (value!.isEmpty) {
-          return error;
-        } else {
-          return null;
-        }
-      },
-      decoration: InputDecoration(
+        controller: controller,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return error;
+          } else {
+            return null;
+          }
+        },
+        decoration: InputDecoration(
           border: const OutlineInputBorder(),
           label: Text(title),
           suffixIcon: isPassword == true
-              ? const Icon(Icons.remove_red_eye_outlined)
-              : const SizedBox()),
-      obscureText: isPassword == true
-          ? isHidden
-              ? false
-              : true
-          : false,
+              ?  IconButton(
+                    onPressed: () {
+                      hideController.isHide.toggle();
+                    },
+                    icon: Icon(
+                      hideController.isHide.value
+                          ? Icons.remove_red_eye_outlined
+                          : Icons.remove_red_eye,
+                    ),
+                  )
+              : const SizedBox(),
+        ),
+        obscureText: isPassword == true ? hideController.isHide.value : false,
+      
     );
   }
 }
