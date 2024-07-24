6295360861520a000bf975278a8643dbb8612d5c
Fitrianto Dwi Cahyo <riandwicahyo28@gmail.com>
2024-07-24 12:04:11 +0700

    feat(ui): add login screen

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 import 'package:shop_test/const/widgets/custom_textfield.dart';
+import 'package:shop_test/controllers/auth_controller.dart';
+import 'package:shop_test/controllers/other_controller/hide_controller.dart';
 import 'package:shop_test/views/screens/auth/register_screen.dart';
+import 'package:shop_test/views/screens/home/home_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   // LoginScreen({super.key});
 
+  final passController = Get.put(PasswordController());
+
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  
+
   TextEditingController email = TextEditingController();
-  TextEditingController password = TextEditingController();
+
+  AuthController authController = Get.put(AuthController());
+
+  login(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      Map<String, dynamic> res = await authController.loginActivity(
+          email.text, passController.cPass.value.text);
+
+      if (res['error'] == false) {
+        Get.to(const HomeScreen());
+        Get.snackbar('Success', res['message'], backgroundColor: Colors.green);
+      } else {
+        Get.snackbar('Error', res['message'], backgroundColor: Colors.red);
+      }
+    } else {
+      Get.snackbar('Attention', 'Tolong Isi Semua',
+          backgroundColor: Colors.amber);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,59 +44,78 @@ class LoginScreen extends StatelessWidget {
         centerTitle: true,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(24),
         child: Form(
           key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomTextfield(
-                title: 'Email Address',
-                isPassword: false,
-                error: 'Email tidak boleh kosong',
-                controller: email,
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              CustomTextfield(
-                title: 'Password',
-                isPassword: true,
-                error: 'Password tidak boleh kosong',
-                controller: password,
-              ),
-              const SizedBox(
-                height: 34,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      print('Validate');
-                      print(email.text);
-                      print(password.text);
-                    } else {
-                      print('Not Validate');
-                    }
-                  },
-                  child: const Text('Sign In'),
+          child: SizedBox(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomTextfield(
+                  title: 'Email Address',
+                  isPassword: false,
+                  error: 'Email tidak boleh kosong',
+                  controller: email,
                 ),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => RegisterScreen(),
+                const SizedBox(
+                  height: 24,
+                ),
+                Obx(
+                  () => TextFormField(
+                    controller: passController.cPass,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Tidak bisa kosong';
+                      } else {
+                        return null;
+                      }
+                    },
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      label: const Text('Password'),
+                      suffixIcon: IconButton(
+                        icon: passController.isHide.value
+                            ? const Icon(Icons.remove_red_eye_outlined)
+                            : const Icon(Icons.remove_red_eye),
+                        onPressed: () {
+                          passController.isHide.toggle();
+                        },
+                      ),
                     ),
-                  );
-                },
-                child: const Text('Register'),
-              ),
-            ],
+                    obscureText: passController.isHide.value ? false : true,
+                  ),
+                ),
+                const SizedBox(
+                  height: 34,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: Obx(
+                    () => ElevatedButton(
+                      onPressed: () {
+                        authController.isLoading.value? null: login(context);
+                      },
+                      autofocus: true,
+                      child: authController.isLoading.value
+                          ? const Padding(
+                              padding: EdgeInsets.all(4.0),
+                              child: CircularProgressIndicator(),
+                            )
+                          : const Text('Sign In'),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                TextButton(
+                  onPressed: () {
+                    Get.to(RegisterScreen());
+                  },
+                  child: const Text('Register'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
